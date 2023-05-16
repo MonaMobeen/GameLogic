d18b710d2a7a8b3a8ddd35e4b141fce6642d93ef
Memoona Mobeen <memoonamobeen618@gmail.com>
2023-05-16 18:35:53 +0500

    NowadaysJustHooks

---
+++ App.js
@@ -0,0 +1,25 @@
+ 
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import CounterApp from './CounterApp';
+import Awesome from './Awesome';
+import HookUseEffect from './HookUseEffect';
+import PractiseReactHooks from './Components/PractiseReactHooks';
+import PractiseForUseEffect from './Components/PractiseForUseEffect';
+function App() {
+  return (
+    <div className="App">
+       <h1>Here's free tutorial of Hooks </h1>
+       <Header/>
+       <Footer/>
+       <CounterApp/>
+       <Awesome/>
+       <HookUseEffect/>
+       <PractiseReactHooks/>
+       <PractiseForUseEffect/>
+    </div>
+  );
+}
+
+export default App;
